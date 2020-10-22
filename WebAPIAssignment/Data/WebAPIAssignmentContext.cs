637741b130801023263256990845ff7e111160b3
Yoshi Tatsumi <YT007@atcltd.atc.org.nz>
2020-10-22 13:14:29 +1300

    Add project files.

---
+++ WebAPIAssignment/Data/WebAPIAssignmentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAssignment.Models;
+
+namespace WebAPIAssignment.Data
+{
+    public class WebAPIAssignmentContext : DbContext
+    {
+        public WebAPIAssignmentContext (DbContextOptions<WebAPIAssignmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIAssignment.Models.TodoItem> TodoItem { get; set; }
+    }
+}
